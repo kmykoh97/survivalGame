089d9b9a364d0782e9b8294e0d4e59879216baf9
KOH MENG YIT <kmykoh97@gmail.com>
2017-08-21 18:02:34 +0800

    Create survivalGame.js

---
+++ survivalGame.js
@@ -0,0 +1,1 @@
+alert("You find yourself inprisoned on an island you cant escape from. \nTry to stay alive as long as possible! \nIf you have suggestions what to improve or ideas for new content (or just a cool highscore ;P), please let it me know in the comments. \nThe game starts at 50 score. \nGL HF! \nFollow me on github:kmykoh97")
