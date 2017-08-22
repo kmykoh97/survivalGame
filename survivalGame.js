978b2ca2773b989172828ae583166a3ce5f0598b
KOH MENG YIT <kmykoh97@gmail.com>
2017-08-22 16:49:14 +0800

    Update survivalGame.js

--- survivalGame.js
+++ survivalGame.js
@@ -1,1 +1,3 @@
+/*JS for page-staring alert only. Rest combined in <script>html</script>*/
+
 alert("You find yourself inprisoned on an island you cant escape from. \nTry to stay alive as long as possible! \nIf you have suggestions what to improve or ideas for new content (or just a cool highscore ;P), please let it me know in the comments. \nThe game starts at 50 score. \nGL HF! \nFollow me on github:kmykoh97")
